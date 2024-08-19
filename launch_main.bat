4ba8e0c4440f7bc3318049bd00a9817c127a616b
Antoine Jourdain de Muizon <antoine.jourdain-de-muizon@engie.com>
2024-08-19 14:25:50 +0200

    [Improvement] On ergonomy of launch_main

--- launch_main.bat
+++ launch_main.bat
@@ -1,11 +1,16 @@
 @echo off
 
 call workon neonat
-echo What is the scenario you want to execute (type a name without space, then type "enter") ?
-set /p NAME=
-echo If you want to force the process, type "--force" ; if not, type "space" + "enter"
-set /p FORCE=
 
+:launch_once_more
+echo What is the scenario you want to execute ? Type a name without space, then type 'enter'
+set "NAME="
+set /p NAME=""
+echo If you want to force the process, type "--force" ; if not, type "space" + "enter"
+set "FORCE="
+set /p FORCE=""
+
+echo %NAME% and %FORCE%
 echo Launch calculation ...
 
 IF NOT EXIST scenarios\%NAME% (
@@ -20,4 +25,18 @@ python neonat_baby_move.py %NAME% %FORCE%
 
 echo Calculation is over.
 
+echo:
+echo If the calculation is unfeasible, please read the log file : scenarios/%NAME%/log_%NAME%.log
+echo ***********************************************************************
+echo:
+SET choice=
+SET /p choice=Launch another calculation ? [press 'Y' + 'enter' if Yes, 'N' if No]: 
+IF '%choice%'=='' GOTO launch_once_more
+IF '%choice%'=='Y' GOTO launch_once_more
+IF '%choice%'=='y' GOTO launch_once_more
+IF '%choice%'=='N' GOTO no
+IF '%choice%'=='n' GOTO no
+
+:no
 pause
+
